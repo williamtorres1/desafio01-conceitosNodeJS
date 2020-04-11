eef4d00ce5209c81d62622a7802fb689fa5d6413
williamtorres1 <williamtorres074@gmail.com>
2020-04-11 13:16:43 -0300

    adicionados logs

--- src/app.js
+++ src/app.js
@@ -9,7 +9,26 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequests(request, response, next){
+  const { method, url } = request
+
+  const logLabel = `[${method.toUpperCase()} ${url}]`
+
+  console.time(logLabel)
+  
+  next()
+  
+  console.timeEnd(logLabel)
+}
+
+app.use(logRequests)
+
 app.get("/repositories", (request, response) => {
+  console.log(`Listing all repositories.`)
+
+  if(repositories.length === 0){
+    console.log(`No repository created.`)
+  }
   return response.json(repositories)
 });
 
@@ -18,7 +37,11 @@ app.post("/repositories", (request, response) => {
 
   const repository = { id: uuid(), title, url, techs, likes: 0}
 
+  console.log(`Creating a new repository...`)
+
   repositories.push(repository)
+
+  console.log(`New repository created.`)
 
   return response.status(200).json(repository)
 });
@@ -30,8 +53,11 @@ app.put("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
   if(repositoryIndex < 0){
+    console.log(`Can't edit this repository.\nRepository not found.`)
     return response.status(400).json({ error: 'Repository not found'})
   }
+
+  console.log(`Editing the repository with id: '${id}'...`)
 
   const repository = {
     id,
@@ -43,6 +69,7 @@ app.put("/repositories/:id", (request, response) => {
 
   repositories[repositoryIndex] = repository
 
+  console.log(`Repository edited!`)
   return response.json(repository)
 });
 
@@ -52,10 +79,15 @@ app.delete("/repositories/:id", (request, response) => {
   const repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
   if(repositoryIndex < 0){
+    console.log(`Can't delete this repository.\nRepository not found.`)
     return response.status(400).json({ error: 'Repository not found'})
   }
 
+  console.log(`Deleting the repository with id: '${id}'...`)
+  
   repositories.splice(repositoryIndex, 1)
+
+  console.log(`Repository deleted!`)
 
   return response.status(204).send()
 });
@@ -64,12 +96,16 @@ app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id)
-  console.log(repositoryIndex)
   if(repositoryIndex < 0){
+    console.log(`Can't like this repository.\nRepository not found.`)
     return response.status(400).json({ error: 'Repository not found'})
   }
 
+  console.log(`Giving like to the repository with id: '${id}'...`)
+
   repositories[repositoryIndex].likes++
+
+  console.log(`Repository liked!`)
   
   return response.status(200).json(repositories[repositoryIndex])
 });
